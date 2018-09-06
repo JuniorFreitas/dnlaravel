52ca20e114705b89f1e115ae18a553a6864695db
juniorfreitas <josedejesusjunior@gmail.com>
2018-09-05 23:56:49 -0300

    complete

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# dnlaravel
-Ambiente configurado com PHP 7.2, Mariadb e NGINX para Laravel
+# DNLaravel
+
+**DNLaravel** é um ambiente pré-configurado com Docker para utilizar 
+
+  - PHP 7.2-alpine
+  - MariaDb
+  - Redis
+  - Nginx
+  
+#### Pré-requisitos
+
+  - Docker e Docker-compose instalado
+  - PHP Instalado
+  - Git Instalado
+  
+### Instalação
+Para instalar basta executar o seguinte comando
+```sh
+sh start.sh
+```
+Após a instalação criará a seguinte pasta **aplication** com a estrutura do projeto Laravel
+###Estruturas das pastas
+```sh
+.
++-- docker
+|   +-- docker-compose.yml
+    |   +-- mysql
+    |   +-- nginx
+        |   +-- certs
+                |-- app.com.key
+                |-- app.com.pem
+        |   +--logs
+            | -- hosts
+            | -- default.conf
+    |   +-- logs
+    |   +-- php
+            | -- php.ini
+            | -- Dockerfile
+    |   +-- sessions
+    |   +-- utils
+            | -- .env
+            | -- composer
++-- application
+-- start.sh
+-- docker-compose.yml
+-- README.md
+-- LICENSE
+```
+> Caso der algum conflito verifique as portas no arquivo **docker-compose.yml** e execute o seguinte comando
+```sh
+docker-compose up -d
+```
+**LICENSE MIT**
--- start.sh
+++ start.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-echo "\033[1;32mDocker com NGINX, Laravel, PHP7.2 e MARIADB - Dynamus TI 2018 \033[0m"
+echo "\033[1;32mDocker com NGINX, Laravel, PHP7.2, Redis e MARIADB - Dynamus TI 2018 \033[0m"
 
 echo Download Laravel
-composer create-project --prefer-dist laravel/laravel application
+php ./docker/utils/composer create-project --prefer-dist laravel/laravel application
 
 echo Copiando arquivo de configuração .env
 cp ./docker/utils/.env ./application/.env
 
 echo Copiando composer
+mkdir ./application/bin
 cp ./docker/utils/composer ./application/bin/composer
 
 echo Instalando as dependencias
@@ -17,25 +18,9 @@ php ./application/bin/composer install
 echo Generate key
 cd application
 php artisan key:generate
-# docker exec -it <app-name>-app php artisan key:generate
 
-# echo Uploading Application container 
-# docker-compose up -d
+echo Startando containers
+cd ..
+docker-compose up -d
 
-# echo Copying the configuration example file
-# docker exec -it <app-name>-app cp .env.example .env
-
-# echo Install dependencies
-# docker exec -it <app-name>-app composer install
-
-# echo Generate key
-# docker exec -it <app-name>-app php artisan key:generate
-
-# echo Make migrations
-# docker exec -it <app-name>-app php artisan migrate
-
-# echo Make seeds
-# docker exec -it <app-name>-app php artisan db:seed
-
-# echo Information of new containers
-# docker ps -a 
+echo "\033[1;32mFim! Dúvidas consulte a documentação https://github.com/JuniorFreitas/dnlaravel \033[0m"
